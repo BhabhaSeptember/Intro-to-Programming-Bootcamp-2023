c2ec9d95f05c10a3da053e28631f8522145e988f
BhabhaSeptember <septemberbhabha@gmail.com>
2023-10-14 16:13:23 +0200

    second commit

---
+++ 2. Intro JS/2. Ch6 (Robot)/2. Ch6.js
@@ -0,0 +1,315 @@
+// // // // // // // "use strict";  //NB: For EVERY JS Program
+
+ // // // // //INVOKING FUNCTIONS
+ // // // // // // nameOfTheFunction();
+ // // // // // // functionThatTakesInput("the input", 5, true);
+
+
+
+ // // // // //WRITING FUNCTIONS (syntax) 
+ // // // // // function nameOfTheFunction() {
+ // // // // //     //content of the function
+ // // // // //     }
+ // // // // // nameOfTheFunction();  
+
+// // // //EXAMPLE 1: 
+ // // // // function sayHello() {
+ // // // //     let user = prompt("What's your name? ");
+ // // // //     console.log("Hello", user + "!");
+ // // // //     }
+
+ // // // //     sayHello();
+
+
+// // //EXAMPLE 2: (function-variable)
+ // // // let varContainingFunction = function() {
+ // // //     let varInFunction = "I'm a variable inside of a function.";
+ // // //     console.log("hi there!", varInFunction);
+ // // //     };
+ // // //     varContainingFunction();
+
+
+ // // //NOTE:
+ // // /*
+ // // Variables : ARE something & contain certain val
+ // // Functions : Are ACTIONS & bundle of statements executed when called
+ // // */
+
+
+ // // /* Practice exercise 6.1 ( DEMO!!! ) */
+//  function addNumbers(x, y) {
+//     return x + y;
+//  }
+//  let x = 2;
+//  let y = 5;
+//  console.log(addNumbers(x, y));
+//  console.log(addNumbers(1, 9));
+
+
+
+ // /* Practice exercise 6.2  ( DEMO!!! ) */   
+//  let adjectives = ["Cheerful", "Bloated", "Greasy", "Warm"];
+//  function theFunction() {
+//      let userInput = prompt("Please enter your name below: ")
+//      let randomAdjective = Math.floor(Math.random() * adjectives.length);
+//      console.log(adjectives[randomAdjective] + " " + userInput);
+//  }
+//  theFunction();
+
+
+
+
+ //PARAMETERS & ARGUMENTS
+ //SYNTAX:
+ // function myFunc(param1, param2) {
+ //     // code of the function;
+ //     }
+   
+
+ //EXAMPLE 1:     
+//  function addTwoNumbers(x, y) {
+//      console.log(x + y);
+//      }
+//      addTwoNumbers(3, 4);
+//      addTwoNumbers(12,-90);
+
+
+
+ /* Practice exercise 6.3     ( DEMO!!! )   */
+ const val1 = 2;
+ const val2 = 3;
+ let operator = "-";
+ function calc(x, y, op) {
+     if (op === "-") {
+         console.log(a - b);
+     } else {
+         console.log(a + b);
+     }
+ }
+ calc(val1, val2, operator);
+
+
+
+
+
+
+
+ //DEFAULT or UNSUITABLE PARAMETERS
+ //EXAMPLE 1: 
+ // addTwoNumbers(); //this means ( 'undefined' + 'undefined' = NaN)
+
+//EXAMPLE 2: 
+ // function addTwoNumbers(x = 2, y = 3) {
+ //     console.log(x + y);
+ //     }
+ //     addTwoNumbers();
+ //     addTwoNumbers(6, 6); //LOGS: 6 + 6
+
+
+ // addTwoNumbers(10); // LOGS: 10 + 3  || x = 10 ; y = 3 (default)
+
+
+ // addTwoNumbers(1,2,3,4); //LOGS: only the first, two arguments are considered
+
+
+
+
+
+
+
+ 
+ //SPECIAL FUNCTIONS & OPERATORS
+//1. ARROW FUNCTIONS
+ /*
+ Different possible layouts
+ a) (param1, param2) => body of the function;
+ b) () => body of the function; 
+ c) param => body of the function;
+
+ d) (param1, param2) => {
+ // line 1;
+ // any number of lines;
+ };
+ */
+
+//EXAMPLE 1: (one param)
+ // let x = ["Driving"]
+ // function doingStuff(x) {
+ //     console.log(x);
+ //     }
+ // doingStuff(x);
+
+ // let doingStuffArrow = (x) => console.log(x); 
+ // doingStuffArrow("Jogging");
+
+
+
+ 
+
+ //EXAMPLE 2: (two param)
+ // let addTwoNumbers = (x, y) => console.log(x + y); 
+ // addTwoNumbers(5, 3); 
+
+
+
+
+
+ //EXAMPLE 3: (no param)
+ // let sayHi = () => console.log("hi"); 
+
+
+
+
+ //EXAMPLE 4: (arrays)
+ // const arr = ["squirrel", "alpaca", "buddy"];
+ // arr.forEach(e => console.log(e));
+
+
+ 
+
+ //2. SPREAD OPERATOR
+ /* 
+ - Number of dots before a referenced expression/string
+ - Spreads out arguments/elements of an array 
+ */
+//EXAMPLE 1: 
+ // let spread = ["so", "much", "fun"];
+ // let message = ["JavaScript", "is", ...spread, "and", "very", "powerful"];
+ // console.log(message);
+
+
+
+
+ //EXAMPLE 2: 
+ // function addTwoNumbers(x, y) {
+ //     console.log(x + y);
+ //     }
+ //     let arr = [5, 9];
+ //     addTwoNumbers(...arr); //instead of "addTwoNumbers(5, 9);"
+
+
+ //EXAMPLE 3:
+ // function addFourNumbers(x, y, z, a) {
+ //     console.log(x + y + z + a);
+ //     }
+ //     let arr = [5, 9];
+ //     let arr2 = [6, 7];
+ //     addFourNumbers(...arr, ...arr2); //instead of "addFourNumbers(5, 9, 6, 7);"
+
+
+
+
+
+
+//REST PARAMETER
+/* 
+- Same as 'Spread Operator' above
+- Symbol (...) is used inside function parameter list NOT in the function call statement 
+*/
+//EXAMPLE 1: 
+//   function someFunction(param1, param2) {
+//     console.log(param1, param2);
+//     }
+//     someFunction("hi", "there!", "How are you?"); 
+
+
+
+//EXAMPLE 2: '...param2' leaves room for more arguments which are converted into an array under 'param2'
+//  function someFunction(param1, ...param2) {
+//      console.log(param1, param2);
+//      }
+//      someFunction("hi", "there!", "How are you?"); //
+
+ //   function someFunction(param1,...param2) {
+ //        console.log(param1, param2);
+ //    }
+ //    someFunction("Hi", "there!", "How are you?", "I am well", "Let's go out", "Where do we go to");
+
+
+
+ /* RETURNING FUNCTION VALUES */ 
+ //EXAMPLE 1: 
+//  let favoriteSubject = prompt("What is your favorite subject?"); //i.e. storing result of 'prompt' function in 'favoriteSubject' variable
+// console.log("Favorite Subject = " + favoriteSubject);
+
+
+//EXAMPLE 2: 
+// function addTwoNumbers(x, y) {
+// console.log(x + y);
+// }
+// let result = addTwoNumber(4, 5);
+// console.log(result);   //Supposed to LOG ( "9" + "undefined" )
+
+
+ // let addTwoNumbers = (x, y) => console.log("1. " + (x + y));
+
+ // let result = addTwoNumbers(4, 5);
+ // console.log( "2. " + result); //outputs defined, nothing inserted to store function of adding two numbers
+
+
+ // function addTwoNumbers(x, y) { //function for the above code 
+ //     return x + y;
+ // }
+
+
+ // let resultsArr = [];
+ // for(let i = 0; i < 10; i ++){
+ // let result = addTwoNumbers(i, 2*i);
+ // resultsArr.push(result);
+ // }
+ // console.log(resultsArr);
+
+
+
+ /* Practice exercise 6.4 */
+ // let emptyArray = [];
+ // for (i = 0; i < emptyArray.length; i++) {
+ //     let val1 = i * 5;
+ //     let val2 = i * i;
+ //     let response = calculate(val1, val2, "+");
+ //     emptyArray.push(response);
+ // }
+ // console.log(emptyArray)
+
+ // function calculate ( x, y, op) {
+ //     if (op === "-") {
+ //         return x - y;
+ //     } else {
+ //         return x + y;
+ //     }
+ // }
+
+
+ /* Returning with arrow functions */
+ // let addTwoNumbers = (x, y) => x + y; 
+ // let result = addTwoNumbers(12, 15); //returning single line function
+ // console.log(result);
+
+
+ // let addTwoNumbers = (x, y) => {
+ //     console.log("Adding...");
+ //     return x + y;  //returning a multi-line function
+ //     }
+
+
+ /* Variable SCOPE in functions */
+ /*  LOCAL VARIABLES in functions */
+
+//  function testAvailability(x) {
+//      console.log("Available here:", x);
+//      }
+//      testAvailability("Hi!");
+//      console.log("Not available here:", x);
+
+    //  function testAvailability() {
+    //      let y = "Local variable!";
+    //      console.log("Available here:", y);
+    //      }
+    //      testAvailability();
+    //      console.log("Not available here:", y);
+     //NOTE: Function paramaters & Local variables inside a function are not available outside of a function (in scope vs out of scope)
+
+
+    //  function testAvailability() {
+    //      let y = "I'll return";
+    
